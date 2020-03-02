d3d938a973e9d15d899691b052d240d7a1d118b5
Patrick Whitehouse <patrick1996@gmail.com>
2020-03-02 20:59:16 +0000

    add animals, dropdown

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
-    const location = "Seattle, WA";
-    return(
-        <div className="search-params">
-            <form>
-                <label htmlFor="location">
-                    Location
-                    <input type="text" id="location" value={location} placeholder="Location" />
-                </label>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    )
-}
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("Dog");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            type="text"
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            name="animal"
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default SearchParams;
